6d4d06ddb44d1c9258a7c3906dbeac5bf149fb35
robinovitch61 <leorobinovitch@gmail.com>
2019-09-06 11:38:59 -0700

    add install files

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the example extension.
+ */
+declare const extension: JupyterFrontEndPlugin<void>;
+export default extension;
